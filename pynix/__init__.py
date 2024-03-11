5fbee73793da84f13fdf0ed9240066155f026519
Pete Dietl <petedietl@gmail.com>
2024-03-11 01:20:55 -0700

    Add computation of store path

--- pynix/__init__.py
+++ pynix/__init__.py
@@ -3,7 +3,7 @@
 import sys
 from pathlib import Path
 from base64 import b64encode
-from pynix.nix import printHash16, printHash32, hash_sha256, nar_serialize
+from pynix.nix import printHash16, printHash32, hash_sha256, nar_serialize, addToStore
 
 import click
 
@@ -23,16 +23,33 @@ def hash() -> None:
 @hash.command()
 @click.option('--base16', is_flag=True)
 @click.option('--base32', is_flag=True)
-@click.argument('paths', type=click.File('rb'), nargs=-1)
+@click.argument('paths', type=click.Path(exists=True, path_type=Path), nargs=-1)
 def file(paths, base16: bool, base32: bool) -> None:
     '''print cryptographic hash of a regular file'''
-    h = hash_sha256(paths[0].read())
-    if base16:
-        click.echo(printHash16(h))
-    elif base32:
-        click.echo(printHash32(h))
-    else:
-        click.echo((b'SHA256-' + b64encode(h)).decode())
+    for p in paths:
+        h = hash_sha256(p.read_bytes())
+        if base16:
+            click.echo(printHash16(h))
+        elif base32:
+            click.echo(printHash32(h))
+        else:
+            click.echo((b'sha256-' + b64encode(h)).decode())
+
+
+@hash.command()
+@click.option('--base16', is_flag=True)
+@click.option('--base32', is_flag=True)
+@click.argument('paths', type=click.Path(exists=True, path_type=Path), nargs=-1)
+def path(paths, base16: bool, base32: bool) -> None:
+    '''print cryptographic hash of the NAR serialization of a path'''
+    for p in paths:
+        h = hash_sha256(nar_serialize(p))
+        if base16:
+            click.echo(printHash16(h))
+        elif base32:
+            click.echo(printHash32(h))
+        else:
+            click.echo((b'sha256-' + b64encode(h)).decode())
 
 
 @cli.group()
@@ -51,6 +68,28 @@ def pack(path):
         sys.exit(1)
 
     sys.stdout.buffer.write(nar_serialize(target_path))
+
+
+@cli.group()
+def store():
+    '''manipulate a Nix store'''
+    pass
+
+
+@store.command()
+@click.argument('path')
+def add(path):
+    '''
+        Add a file or directory to the Nix addToStore
+        (This is just a dry-run operation)
+    '''
+
+    target_path = Path(path)
+    if not target_path.exists:
+        click.echo(f'Path {target_path} does not exist!', err=True)
+        sys.exit(1)
+
+    print(addToStore(target_path, target_path.name))
 
 
 if __name__ == '__main__':
--- pynix/nix.py
+++ pynix/nix.py
@@ -4,6 +4,8 @@ import stat
 from collections.abc import Callable
 import functools
 
+NIX_STORE = b'/nix/store'
+
 
 def hash_md5(c: bytes) -> bytes:
     return hashlib.md5(c).digest()
@@ -24,13 +26,12 @@ def printHash16(c: bytes) -> str:
 def printHash32(c: bytes) -> str:
     digits32 = '0123456789abcdfghijklmnpqrsvwxyz'
 
-    n = len(c) * 8 // 5
+    n = (len(c) * 8 - 1) // 5 + 1
 
     x = int.from_bytes(c, 'little')
     s = ''
-    while n >= 0:
+    for n in range(n - 1, -1, -1):
         s += digits32[x // 32 ** n % 32]
-        n -= 1
 
     return s
 
@@ -101,3 +102,20 @@ def _nar_int(n: int) -> bytes:
 
 def _nar_pad(s: bytes) -> bytes:
     return s + (b'\x00' * ((8 - (len(s) & 7)) & 7))
+
+
+def makePath(path_type: str, descr: str, name: str) -> Path:
+    assert path_type in {'source'}
+
+    s = path_type.encode('ascii') + b':sha256:' + descr.encode() + \
+        b':' + NIX_STORE + b':' + name.encode('ascii')
+    return Path(NIX_STORE.decode()) / \
+        Path(printHash32(truncate(20, hash_sha256(s))) +
+             '-' + name)
+
+
+def addToStore(fso: Path, name: str, refs: list[Path] = []) -> Path:
+    c = nar_serialize(fso)
+    h = hash_sha256(c)
+    p = makePath('source', printHash16(h), name)
+    return p
